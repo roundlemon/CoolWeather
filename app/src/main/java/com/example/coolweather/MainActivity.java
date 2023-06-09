c34c4540431e387b02789f620b40bb1bccdcb620
limengyuan <limengyuan18@meituan.com>
2023-06-09 11:14:04 +0800

    添加数据库表

--- app/src/main/java/com/example/coolweather/MainActivity.java
+++ app/src/main/java/com/example/coolweather/MainActivity.java
@@ -4,12 +4,14 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 
+import org.litepal.LitePal;
+
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
+        LitePal.initialize(this);
     }
 }
